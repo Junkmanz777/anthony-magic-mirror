d02306cd151659ffcef145df36ab0251f07e88b9
Junkmanz777 <anthonyspam7777@gmail.com>
2026-08-13 13:36:27 -0600

    Add update script for Anthony Magic Mirror
    The script updates the Anthony Magic Mirror project by pulling the latest changes from Git, synchronizing custom modules, checking the configuration, and restarting the MagicMirror service.

--- update.sh
+++ update.sh
@@ -1,1 +1,28 @@
+#!/bin/bash
 
+set -e
+
+PROJECT_DIR="$HOME/anthony-magic-mirror"
+MM_DIR="$HOME/MagicMirror"
+
+echo "Updating Anthony Magic Mirror..."
+
+cd "$PROJECT_DIR"
+git pull
+
+echo "Updating custom modules..."
+
+rsync -av \
+  "$PROJECT_DIR/modules/" \
+  "$MM_DIR/modules/"
+
+echo "Checking configuration..."
+
+cd "$MM_DIR"
+node --run config:check
+
+echo "Restarting MagicMirror..."
+
+systemctl --user restart magicmirror.service
+
+echo "Update complete."
